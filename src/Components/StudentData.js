1d78d257cdb3df3098beae40c69370d9c12db184
JannatRakhi <jannatrakhi31155@gmail.com>
2021-12-02 23:22:52 +0600

    first commit

---
+++ src/Components/StudentData.js
@@ -0,0 +1,86 @@
+const StudentData =[
+    {
+        id: 1,
+        name: "Nura Jannat Rakhi",
+        studentId: "19-40042-1",
+        batch: "19-1",
+        cgpa: "3.85",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 2,
+        name: "Fahmida Rashid",
+        studentId: "19-40040-1",
+        batch: "19-1",
+        cgpa: "3.90",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 3,
+        name: "Mehedi Hassan",
+        studentId: "19-40030-1",
+        batch: "19-1",
+        cgpa: "3.95",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 4,
+        name: "Kawser Hamid",
+        studentId: "19-40074-1",
+        batch: "19-1",
+        cgpa: "3.75",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 5,
+        name: "Fardin hasan",
+        studentId: "19-40043-1",
+        batch: "19-1",
+        cgpa: "3.65",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 6,
+        name: "Parag ghosh",
+        studentId: "19-40085-1",
+        batch: "19-1",
+        cgpa: "3.65",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 7,
+        name: "Hamadi Hamid",
+        studentId: "19-40087-1",
+        batch: "19-1",
+        cgpa: "3.75",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 8,
+        name: "Mishal hasan",
+        studentId: "19-40089-1",
+        batch: "19-1",
+        cgpa: "3.65",
+        address: "Dhaka",
+        department: "CSE"
+    },
+    {
+        id: 9,
+        name: "Mahira Khan",
+        studentId: "19-40080-1",
+        batch: "19-1",
+        cgpa: "3.65",
+        address: "Dhaka",
+        department: "CSE"
+    }
+
+
+]
+export default StudentData;
